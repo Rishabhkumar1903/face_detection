160e8e9f5a8031a77b7e20f979e43ac6a8638c4f
Rishabhkumar1903 <rishabhverma190388099@gmail.com>
2025-08-20 02:02:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 def detect_faces_in_image(image, max_width=2000):
     list_faces = model.detectMultiScale(image)
     for x, y, w, h in list_faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 4)
 
     # ✅ Convert BGR → RGB before returning
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -33,3 +33,4 @@ def detect_faces_in_video(video_path, max_width=600):   # video aur chhota
             frame = cv2.resize(frame, (int(width * scale), int(height * scale)))
         yield frame
     cap.release()
+
