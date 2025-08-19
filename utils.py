b60c92ca20efae3c551757cc3785d0bcfe0e848d
Rishabhkumar1903 <rishabhverma190388099@gmail.com>
2025-08-20 01:40:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2
 
+# Load model
 model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # ------------------ IMAGE DETECTION ------------------
@@ -12,8 +13,9 @@ def detect_faces_in_image(image, max_width=2000):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
+
 # ------------------ VIDEO DETECTION ------------------
-def detect_faces_in_video(video_path, max_width=400):   # <-- video aur chhota
+def detect_faces_in_video(video_path, max_width=600):   # video aur chhota
     cap = cv2.VideoCapture(video_path)
     while True:
         ret, frame = cap.read()
@@ -29,27 +31,5 @@ def detect_faces_in_video(video_path, max_width=400):   # <-- video aur chhota
         if width > max_width:
             scale = max_width / width
             frame = cv2.resize(frame, (int(width * scale), int(height * scale)))
-        yield frame
-    cap.release()
-
-# ------------------ WEBCAM DETECTION ------------------
-def detect_faces_in_webcam(detect=True, max_width=500):
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        if detect:  # ✅ detect toggle ON/OFF
-            list_faces = model.detectMultiScale(frame)
-            for x, y, w, h in list_faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-        # Resize frame
-        height, width = frame.shape[:2]
-        if width > max_width:
-            scale = max_width / width
-            frame = cv2.resize(frame, (int(width * scale), int(height * scale)))
-
         yield frame
     cap.release()
